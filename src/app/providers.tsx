07b5bbdbc3819b021d1a44a9bd719e35928e5cbf
Ngoc Thach <dreon1008@gmail.com>
2023-08-08 10:56:12 +0700

    folder structure

---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+import { Toaster } from 'react-hot-toast';
+
+import { TOAST_DEFAULT_OPTIONS } from '@/config/helpers/toast.helper';
+
+const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {children}
+      <Toaster {...TOAST_DEFAULT_OPTIONS} />
+    </>
+  );
+};
