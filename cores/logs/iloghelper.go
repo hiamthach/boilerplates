fa1ca395898380c7cc857091ba8d728daf24dca1
Ngoc Thach <dreon1008@gmail.com>
2023-08-14 16:56:39 +0700

    log helper

---
+++ cores/logs/iloghelper.go
@@ -0,0 +1,39 @@
+package cores_logs
+
+import (
+	"go-microservices/app/share"
+
+	"github.com/sirupsen/logrus"
+)
+
+type SessionContext struct {
+	Id             string `json:"Id,omitempty" bson:"Id,omitempty"`
+	UserAgent      string `json:"UserAgent,omitempty" bson:"UserAgent,omitempty"`
+	ClientCode     string `json:"ClientCode,omitempty" bson:"ClientCode,omitempty"`
+	UserId         string `json:"UserId,omitempty" bson:"UserId,omitempty"`
+	UserName       string `json:"UserName,omitempty" bson:"UserName,omitempty"`
+	SessionID      string `json:"SessionID,omitempty" bson:"SessionID,omitempty"`
+	ClientIP       string `json:"ClientIp,omitempty" bson:"ClientIp,omitempty"`
+	AppVersion     string `json:"AppVersion,omitempty" bson:"AppVersion,omitempty"`
+	ClientVersion  string `json:"ClientVersion,omitempty" bson:"ClientVersion,omitempty"`
+	ClientPlatform string `json:"ClientPlatform,omitempty" bson:"ClientPlatform,omitempty"`
+	ClientBundle   string `json:"ClientBundle,omitempty" bson:"ClientBundle,omitempty"`
+	ShardID        int32  `json:"ShardID,omitempty" bson:"ShardID,omitempty"`
+	IsAdmin        bool   `json:"IsAdmin,omitempty" bson:"IsAdmin,omitempty"`
+}
+
+type ILogHelper interface {
+	Instance(lg *logrus.Logger)
+	GetLogger() *logrus.Logger
+	GetEntry(context *SessionContext, logData *share.AuthSvcLog) *logrus.Entry
+	GetLiteEntry(context *SessionContext, action string) *logrus.Entry
+	InfoMessage(context *SessionContext, action string, messsage string)
+	ErrorMessage(context *SessionContext, messsage string)
+	Error(context *SessionContext, err error)
+	SysError(err error)
+	WarnMessage(context *SessionContext, messsage string)
+	WriteLog(lv logrus.Level, context *SessionContext, logData *share.AuthSvcLog)
+	WriteFinishLog(lv logrus.Level, context *SessionContext, logData *share.AuthSvcLog, message string)
+	StartLog(context *SessionContext, action string, isValidError bool) *share.AuthSvcLog
+	Duration(log share.Action)
+}
